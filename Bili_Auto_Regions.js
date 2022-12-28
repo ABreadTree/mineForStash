29641b5110be20b6a09c44056f88eb0e63543640
ABreadTree <62497798+ABreadTree@users.noreply.github.com>
2022-12-28 11:44:40 +0800

    Update Bili_Auto_Regions.js
    for stash

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -55,11 +55,11 @@ let $ = nobyda();
 let run = EnvInfo();
 
 async function SwitchRegion(play) {
-	const Group = $.read('BiliArea_Policy') || '📺 Bilibili'; //Your blibli policy group name.
+	const Group = $.read('BiliArea_Policy') || 'Bilibili'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
-	const TW = $.read('BiliArea_TW') || '🇨🇳Taiwan'; //Your Taiwan sub-policy name.
-	const HK = $.read('BiliArea_HK') || '🇭🇰Taiwan'; //Your HongKong sub-policy name.
-	const DF = $.read('BiliArea_DF') || '🇨🇳Taiwan'; //Sub-policy name used after region is blocked(e.g. url 404)
+	const TW = $.read('BiliArea_TW') || 'TW 台湾'; //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') || 'TW 台湾'; //Your HongKong sub-policy name.
+	const DF = $.read('BiliArea_DF') || 'TW 台湾'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
